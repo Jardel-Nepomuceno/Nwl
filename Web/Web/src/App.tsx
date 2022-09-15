9d2cd55ff57bbf8a3cb7c92bf952a4e9c468444e
jardel <jardelsantos15@gmail.com>
2022-09-14 21:45:40 -0300

    dia 2

--- Web/Web/src/App.tsx
+++ Web/Web/src/App.tsx
@@ -1,27 +1,47 @@
-// Componentes / Propriedades
+import "./styles/main.css";
 
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-   return ( 
-    <button>
-      {props.title}
-    </button>
-    
-  )
-}
+import logoImg from "./assets/logo-nlw-esports.svg";
 
 function App() {
-  return(
-<div>
-  <Button title="Send 1" />
-  <Button title="Send 2" />
-  <Button title="Send 3" />
-</div>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>
+        está aqui.
+      </h1>
 
-  )
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="game-1.png" alt="" />
+          <div className="w-full pt 16 pb-4 px-4 bg-game-gradient absolute bottom-0  left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="game-2.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="game-3.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="game-4.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="game-5.png" alt="" />
+        </a>
+        <a href="" className="relative">
+          <img src="game-6.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
